e299b3eec2e0a76a2986d5f700f357770dba855c
bensagi <73578662+bensagi@users.noreply.github.com>
2024-10-07 14:13:59 +0300

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -20,12 +20,6 @@ variable "prefix_separator" {
 # Cluster
 ################################################################################
 
-variable "cluster_name" {
-  description = "Name of the EKS cluster"
-  type        = string
-  default     = ""
-}
-
 variable "cluster_version" {
   description = "Kubernetes `<major>.<minor>` version to use for the EKS cluster (i.e.: `1.27`)"
   type        = string
